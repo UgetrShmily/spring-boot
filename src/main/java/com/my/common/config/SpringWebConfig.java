aee2b1e31591c3d098ff29480e740c664ca560e7
Shmily <1939179265@qq.com>
2022-06-20 21:29:48 +0800

    spring boot can run

--- src/main/java/com/my/common/config/SpringWebConfig.java
+++ src/main/java/com/my/common/config/SpringWebConfig.java
@@ -1,10 +1,9 @@
 package com.my.common.config;
 
+import com.my.common.web.TimeAccessInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.my.common.web.TimeAccessInterceptor;
 
 @Configuration
 public class SpringWebConfig implements WebMvcConfigurer{
--- src/main/java/com/my/controller/PageController.java
+++ src/main/java/com/my/controller/PageController.java
@@ -1,20 +1,20 @@
 package com.my.controller;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.util.StringUtils;
+import com.my.pojo.SysUser;
+import org.apache.shiro.web.filter.mgt.DefaultFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.my.pojo.SysUser;
 @Controller
 @RequestMapping("/")
 public class PageController {
 	@RequestMapping("doIndexUI")
 	public String doIndexUI(Model model) {
-		SysUser user=(SysUser)SecurityUtils.getSubject().getPrincipal();
-		model.addAttribute("user", user);
+//		SysUser user=(SysUser)SecurityUtils.getSubject().getPrincipal();
+		SysUser user = new SysUser();
+		model.addAttribute("user", DefaultFilter.user);
 		return "starter";
 	}
 	@RequestMapping("/{module}/{modulePage}")
